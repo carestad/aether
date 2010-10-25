680e1904527d1a4244b0a6dc22fa060acc19fb64
Alexander Karlstad <alexander@karlstad.be>
2010-10-26 00:50:19 +0200

    Removed unnecessary and old comment

--- sections/HTTP.php
+++ sections/HTTP.php
@@ -1,17 +1,4 @@
 <?php
-
-/**
- * 
- * Generic section, only serves up modules and does nothing.
- * Should mainly be used for prototyping and testing
- * but also totaly generic services like login and logout
- * can use this, unless the context requires some sort of logging
- * 
- * Created: 2007-04-23
- * @author Raymond Julin
- * @package aether.sections
- */
-
 class HTTP extends AetherSection {
     
     /**
